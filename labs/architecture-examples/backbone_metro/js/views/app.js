f7c1a2ace560043dc2fa1fa5f98c95dc76944fd8
Timo Uhlmann <t.uhlmann@zeroseven.de>
2013-06-19 05:05:03 -0700

    Re-enabled filtering of Todos.

--- labs/architecture-examples/backbone_metro/js/views/app.js
+++ labs/architecture-examples/backbone_metro/js/views/app.js
@@ -45,9 +45,9 @@ $(function( $ ) {
 			};
 
 			/*this.listenTo(app.Todos, 'add', this.addOne);
-			this.listenTo(app.Todos, 'reset', this.addAll);
+			this.listenTo(app.Todos, 'reset', this.addAll);*/
 			this.listenTo(app.Todos, 'change:completed', this.filterOne);
-			this.listenTo(app.Todos, 'filter', this.filterAll);*/
+			this.listenTo(app.Todos, 'filter', this.filterAll);
 			this.listenTo(app.Todos, 'all', this.render);
 
 			app.Todos.fetch();
